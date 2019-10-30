ebf6aa4d7d953596fdaf107dc0f69638139abf36
Quinn BP <qbarkerp@gmail.com>
2019-10-29 18:17:13 -0700

    He Boing

--- GAME230-Pong/GAME230-Pong/main.cpp
+++ GAME230-Pong/GAME230-Pong/main.cpp
@@ -6,25 +6,114 @@
 #include <SFML/OpenGL.hpp>
 #include <SFML/Main.hpp>
 
+#include<vector>
+
+using namespace std;
+using namespace sf;
+
+const int WINDOW_WIDTH = 512;
+const int WINDOW_HEIGHT = 512;
+
+class Paddle {
+public:
+	Paddle();
+	Paddle(Vector2f position, Vector2f velocity);
+	void draw(RenderWindow* window);
+	void update(float dt);
+private:
+	Vector2f velocity;
+	Vector2f position;
+};
+
+class Ball {
+public:
+	Ball();
+	Ball(Vector2f position, Vector2f velocity);
+	void draw(RenderWindow* window);
+	void update(float dt);
+private:
+	Vector2f velocity;
+	Vector2f position;
+	CircleShape shape;
+	int radius;
+};
+
+Ball::Ball(Vector2f position, Vector2f velocity) {
+	// set up shape
+	this->radius = 10;
+	this->shape = CircleShape(this->radius);
+	this->shape.setFillColor(Color::Blue);
+
+	// set up position and velocity
+	this->position = position;
+	this->velocity = velocity;
+}
+
+Ball::Ball() {
+	Ball::Ball(Vector2f(0.0f, 0.0f), Vector2f(0.0f, 0.0f));
+}
+
+void Ball::update(float dt) {
+	this->position.x += this->velocity.x * dt;
+	this->position.y += this->velocity.y * dt;
+
+	// check x bounds
+	if (this->position.x > WINDOW_WIDTH - 2 * this->radius) {
+		this->position.x = WINDOW_WIDTH - 2 * this->radius;
+		this->velocity.x *= -1;
+	} 
+	else if (this->position.x < 0) {
+		this->position.x = 0;
+		this->velocity.x *= -1;
+	}
+
+	// check y bounds
+	if (this->position.y > WINDOW_HEIGHT - 2 * this->radius) {
+		this->position.y = WINDOW_HEIGHT - 2 * this->radius;
+		this->velocity.y *= -1;
+	}
+	else if (this->position.y < 0) {
+		this->position.y = 0;
+		this->velocity.y *= -1;
+	}
+}
+
+void Ball::draw(RenderWindow* window) {
+	this->shape.setPosition(this->position);
+	window->draw(this->shape);
+}
+
+
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+	RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Pong"); // create window
+	window.setVerticalSyncEnabled(true); // vsync bc why not
 
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	Clock clock; // init clock
+	float dt_ms = 0;
 
-	while (window.isOpen())
+	Ball ball(Vector2f(100.0f, 0.0f), Vector2f(1.0f, 1.0f)); // initialize game objects
+
+	while (window.isOpen()) // overall game loop
 	{
-		sf::Event event;
+		// Timing
+		dt_ms = clock.getElapsedTime().asMilliseconds();
+		clock.restart();
+		
+		Event event;
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::Closed)
+			if (event.type == Event::Closed) // check if window was closer this cycle
 				window.close();
 		}
 
-		window.clear();
-		window.draw(shape);
-		window.display();
+		// update functions
+		ball.update(dt_ms); // update the movement of the ball
+
+		// draw functions
+		window.clear(); // clear to black (no epilepsy warnings)
+		ball.draw(&window); // draw updated game objects
+		window.display(); // draw the new screen
 	}
 
 	return 0;
